8552493d40307ef32f938965ae9716a4b34afab5
Albresky <albre02@outlook.com>
2026-01-04 08:28:47 +0000

    [02] Matrix Multiplication: Remove WMMA impl

--- CUDA/02-matmul/README.md
+++ CUDA/02-matmul/README.md
@@ -15,7 +15,12 @@ Matrix sizes
 
 | Kernel Version | Avg Latency (ms) | Memory Bandwidth (GB/s) | Compute (GFLOPS) |
 | :--- | :--- | :--- | :--- |
-| **Naive (__K0)** | 0.4397 | 28.4940 |  4862.9793 |
-| **Smem (__K1)** | 0.3431 | 36.6695 | 6258.2574 |
-| **GemmPerThread + Smem (__K2)** | 0.0956 | 131.5632 | 22453.4480 |
+| **(__K0) Naive** | 0.4397 | 28.4940 |  4862.9793 |
+| **(__K1) Smem** | 0.3431 | 36.6695 | 6258.2574 |
+| **(__K2) GemmPerThread + Smem** | 0.0956 | 131.5632 | 22453.4480 |
+
+### 分析
+
+- 1. 关于线程块的二维形状
+目前是 blockDim.x == blockDim.y 的方形设计。由于我们希望能够对原始矩阵和转置矩阵的列访存都实现最大化 memory coalescing，那么一旦他们不相等，将会顾此失彼。
 
--- CUDA/02-matmul/matmul.cu
+++ CUDA/02-matmul/matmul.cu
@@ -1,6 +1,6 @@
 #include <cuda_runtime.h>
 
-#define __K3
+#define __K2
 
 /* 0. naive impl */
 #ifdef __K0
@@ -140,109 +140,6 @@ __global__ void matrix_multiplication_kernel(const float *A, const float *B,
 }
 #endif
 
-/* 3. using 4x4 WMMA and smem for tiling */
-/**
- * WARNING: This impl WON'T PASS in LeetGPU OJ due to the low precision of TF32. 
- * In PTX programming model, we usually use `K` as the reducing dim, however,
- * in questions of LeetGPU I find most reducing dimension is dim `N`.
-*/
-#ifdef __K3
-#include <mma.h>
-using namespace nvcuda;
-
-__global__ void matrix_multiplication_kernel(const float *A, const float *B,
-                                             float *C, int M, int N, int K)
-{
-  const int BM = 32;
-  const int BN = 16;
-  const int BK = 64;
-
-  const int WMMA_M = 16;
-  const int WMMA_N = 16;
-  const int WMMA_K = 8;
-  const int WARP_DIM = 32;
-
-  // 2x4 warps
-  const int WARPS_N = 4;
-
-  // 1D thread id
-  int tid = threadIdx.y * blockDim.x + threadIdx.x;
-  int warpId = tid / WARP_DIM;
-
-  int warpRow = warpId / WARPS_N;
-  int warpCol = warpId % WARPS_N;
-
-  int blockRow = blockIdx.y * BM;
-  int blockCol = blockIdx.x * BK;
-
-  __shared__ float s_A[BM][BN];
-  __shared__ float s_B[BN][BK];
-  __shared__ float s_C[BM][BK];
-
-  wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, wmma::precision::tf32, wmma::row_major> a_frag;
-  wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, wmma::precision::tf32, wmma::row_major> b_frag;
-  wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, float> c_frag;
-  wmma::fill_fragment(c_frag, 0.0f);
-
-  for (int n_idx = 0; n_idx < N; n_idx += BN) {
-    // G2S
-    #pragma unroll
-    for (int i = 0; i < 2; ++i) {
-      int idx = tid + i * 256;
-      if (idx < BM * BN) {
-        int r = idx / BN;
-        int c = idx % BN;
-        if (blockRow + r < M && n_idx + c < N) {
-          s_A[r][c] = A[(blockRow + r) * N + (n_idx + c)];
-        } else {
-          s_A[r][c] = 0.0f;
-        }
-      }
-    }
-
-    #pragma unroll
-    for (int i = 0; i < 4; ++i) {
-      int idx = tid + i * 256;
-      if (idx < BN * BK) {
-        int r = idx / BK;
-        int c = idx % BK;
-        if (n_idx + r < N && blockCol + c < K) {
-          s_B[r][c] = B[(n_idx + r) * K + (blockCol + c)];
-        } else {
-          s_B[r][c] = 0.0f;
-        }
-      }
-    }
-    __syncthreads();
-
-    // wmma
-    for (int k_step = 0; k_step < BN; k_step += WMMA_K) {
-        wmma::load_matrix_sync(a_frag, &s_A[warpRow * 16][k_step], BN);
-        wmma::load_matrix_sync(b_frag, &s_B[k_step][warpCol * 16], BK);
-
-        // MMA
-        wmma::mma_sync(c_frag, a_frag, b_frag, c_frag);
-    }
-    __syncthreads();
-  }
-  wmma::store_matrix_sync(&s_C[warpRow * 16][warpCol * 16], c_frag, BK, wmma::mem_row_major);
-  
-  __syncthreads();
-
-  #pragma unroll
-  for (int i = 0; i < 8; ++i) {
-    int idx = tid + i * 256;
-    if (idx < BM * BK) {
-      int r = idx / BK;
-      int c = idx % BK;
-      if (blockRow + r < M && blockCol + c < K) {
-        C[(blockRow + r) * K + (blockCol + c)] = s_C[r][c];
-      }
-    }
-  }
-}
-#endif
-
 // A, B, C are device pointers (i.e. pointers to memory on the GPU)
 extern "C" void solve(const float *A, const float *B, float *C, int M, int N,
                       int K) {
@@ -253,10 +150,6 @@ extern "C" void solve(const float *A, const float *B, float *C, int M, int N,
   dim3 blocksPerGrid(
       (K + threadsPerBlock.x * BLK - 1) / (threadsPerBlock.x * BLK),
       (M + threadsPerBlock.y * BLK - 1) / (threadsPerBlock.y * BLK));
-#elif defined(__K3)
-  dim3 blocksPerGrid(
-      (K + 64 - 1) / 64,
-      (M + 32 - 1) / 32);
 #else
   dim3 blocksPerGrid((K + threadsPerBlock.x - 1) / threadsPerBlock.x,
                      (M + threadsPerBlock.y - 1) / threadsPerBlock.y);
