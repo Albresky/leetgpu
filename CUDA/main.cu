a42a2ec4abcd2b4abcc5acb93a4628f00670d3af
Albresky <albre02@outlook.com>
2026-01-23 14:37:47 +0800

    [CUDA] Specialize iterations for benchmark and profiling

--- CUDA/main.cu
+++ CUDA/main.cu
@@ -1,67 +1,72 @@
-#include "common.h"
 #include <memory>
+#include "common.h"
 
-int main(int argc, char **argv) {
-    printf("Running global main for %s...\n", argv[0]);
+int main(int argc, char** argv)
+{
+  printf("Running global main for %s...\n", argv[0]);
 
-    std::unique_ptr<Problem> problem(create_problem());
+  std::unique_ptr<Problem> problem(create_problem());
 
-    problem->init();
+  problem->init();
 
-    constexpr int WARMUP_ITER = 5;
-    constexpr int TEST_ITER = 20;
+#ifdef __NCU_ON__
+  constexpr int WARMUP_ITER = 0;
+  constexpr int TEST_ITER   = 1;
+#else
+  constexpr int WARMUP_ITER = 5;
+  constexpr int TEST_ITER   = 20;
+#endif
+  printf("Warming up...\n");
+  for (int i = 0; i < WARMUP_ITER; ++i) {
+    problem->run();
+  }
+  CHECK(cudaDeviceSynchronize());
 
-    printf("Warming up...\n");
-    for (int i = 0; i < WARMUP_ITER; ++i) {
-        problem->run();
-    }
-    CHECK(cudaDeviceSynchronize());
+  cudaEvent_t start, stop;
+  CHECK(cudaEventCreate(&start));
+  CHECK(cudaEventCreate(&stop));
 
-    cudaEvent_t start, stop;
-    CHECK(cudaEventCreate(&start));
-    CHECK(cudaEventCreate(&stop));
+  float total_time = 0.0f;
+  float min_time   = 1e9f;
+  float max_time   = 0.0f;
 
-    float total_time = 0.0f;
-    float min_time = 1e9f;
-    float max_time = 0.0f;
+  printf("Running benchmark (%d iterations)...\n", TEST_ITER);
 
-    printf("Running benchmark (%d iterations)...\n", TEST_ITER);
+  for (int i = 0; i < TEST_ITER; ++i) {
+    float milliseconds = 0;
+    CHECK(cudaEventRecord(start));
+    problem->run();
+    CHECK(cudaEventRecord(stop));
+    CHECK(cudaEventSynchronize(stop));
+    CHECK(cudaEventElapsedTime(&milliseconds, start, stop));
 
-    for (int i = 0; i < TEST_ITER; ++i) {
-        float milliseconds = 0;
-        CHECK(cudaEventRecord(start));
-        problem->run();
-        CHECK(cudaEventRecord(stop));
-        CHECK(cudaEventSynchronize(stop));
-        CHECK(cudaEventElapsedTime(&milliseconds, start, stop));
+    total_time += milliseconds;
+    if (milliseconds < min_time) min_time = milliseconds;
+    if (milliseconds > max_time) max_time = milliseconds;
+  }
 
-        total_time += milliseconds;
-        if (milliseconds < min_time) min_time = milliseconds;
-        if (milliseconds > max_time) max_time = milliseconds;
-    }
+  float avg_time = total_time / TEST_ITER;
 
-    float avg_time = total_time / TEST_ITER;
-    
-    long long bytes = problem->get_bytes();
-    long long flops = problem->get_flops();
-    
-    double mem_bandwidth = (double)bytes / (avg_time * 1e-3) / 1e9; // GB/s
-    double gflops = (double)flops / (avg_time * 1e-3) / 1e9; // GFLOPS
+  long long bytes = problem->get_bytes();
+  long long flops = problem->get_flops();
 
-    printf("\nPerformance Metrics:\n");
-    printf("  Total Time:     %-10.4f ms\n", total_time);
-    printf("  Avg Latency:    %-10.4f ms\n", avg_time);
-    printf("  Min Latency:    %-10.4f ms\n", min_time);
-    printf("  Max Latency:    %-10.4f ms\n", max_time);
-    printf("  Mem Bandwidth:  %-10.4f GB/s\n", mem_bandwidth);
-    printf("  Throughput:     %-10.4f GFLOPS\n", gflops);
-    printf("\n");
+  double mem_bandwidth = (double)bytes / (avg_time * 1e-3) / 1e9;  // GB/s
+  double gflops        = (double)flops / (avg_time * 1e-3) / 1e9;  // GFLOPS
 
-    CHECK(cudaEventDestroy(start));
-    CHECK(cudaEventDestroy(stop));
-    CHECK(cudaGetLastError()); 
+  printf("\nPerformance Metrics:\n");
+  printf("  Total Time:     %-10.4f ms\n", total_time);
+  printf("  Avg Latency:    %-10.4f ms\n", avg_time);
+  printf("  Min Latency:    %-10.4f ms\n", min_time);
+  printf("  Max Latency:    %-10.4f ms\n", max_time);
+  printf("  Mem Bandwidth:  %-10.4f GB/s\n", mem_bandwidth);
+  printf("  Throughput:     %-10.4f GFLOPS\n", gflops);
+  printf("\n");
 
-    problem->verify();
+  CHECK(cudaEventDestroy(start));
+  CHECK(cudaEventDestroy(stop));
+  CHECK(cudaGetLastError());
 
-    return 0;
+  problem->verify();
+
+  return 0;
 }
